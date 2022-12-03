5a1222596f6fe44cac24fbe398c58b5da2debf51
ahmadibrahimtibin <ahmadtibin@gmail.com>
2022-12-03 14:27:00 +0200

    fixed old issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "adec0ca1d1ab3fd3aa21e22cac56ddc3",
 "icons/Icon-maskable-192.png": "71d866258d4216394640522c1c85824d",
 "icons/Icon-maskable-512.png": "adec0ca1d1ab3fd3aa21e22cac56ddc3",
-"index.html": "738f5ce04b2f4e3a50e9b8cca6c2271c",
-"/": "738f5ce04b2f4e3a50e9b8cca6c2271c",
-"main.dart.js": "1493ccd57d00b19f73cf8b130b2cba14",
+"index.html": "236e5c0e9ee552b7e9b85e7a19d58379",
+"/": "236e5c0e9ee552b7e9b85e7a19d58379",
+"main.dart.js": "cb61a172ab75318830dbcb390be86681",
 "manifest.json": "15164bcfaf81fca50e7e5778f1bcd82d",
 "version.json": "e2bdc87ae1592b7844dc9d9b7b926612"
 };
